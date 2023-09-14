5392662063b5463d3ad49cf5cc50a492ad88d2a7
Max Kotyk <mxm.kotyk@gmail.com>
2023-09-14 14:02:00 +0200

    P3 - Fixed container behavior on button click

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,14 +1,19 @@
 const openBtn = document.getElementById("open");
 const closeBtn = document.getElementById("close");
 const container = document.querySelector(".container");
+const body = document.querySelector("body");
+const content = document.querySelector(".content");
 
 const handleClick = (e) => {
   const clickedBtn = e.currentTarget.id;
   console.log(clickedBtn);
   if (clickedBtn === "open") {
-    container.classList.add("show-nav");
+    container.classList.add("show-nav", "no-scroll", "enlarged");
   } else if (clickedBtn === "close") {
-    container.classList.remove("show-nav");
+    container.classList.remove("show-nav", "no-scroll");
+    setTimeout(() => {
+      container.classList.remove("enlarged");
+    }, 500);
   }
 };
 
